1e18c41c3904fbe30e1859ec56648b06ccfb1f6a
Daniil Davidovich <dd.jopgm@gmail.com>
2023-04-10 17:52:13 +0300

    Initial Commit

---
+++ Home Work 23/Home_Work_23App.swift
@@ -0,0 +1,17 @@
+//
+//  Home_Work_23App.swift
+//  Home Work 23
+//
+//  Created by Daniil Davidovich on 10.04.23.
+//
+
+import SwiftUI
+
+@main
+struct Home_Work_23App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
