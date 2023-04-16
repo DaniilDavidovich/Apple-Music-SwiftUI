eafce42017eefe5c734462a750ea90ecc2aa3272
Daniil Davidovich <dd.jopgm@gmail.com>
2023-04-17 01:25:36 +0300

    create StationVIew

---
+++ AppleMusic SwiftUI/Modules/ModuleRadio/Views/StationsView.swift
@@ -0,0 +1,50 @@
+//
+//  StationsView.swift
+//  AppleMusic SwiftUI
+//
+//  Created by Daniil Davidovich on 17.04.23.
+//
+
+import SwiftUI
+
+struct StationsView: View {
+    
+    let model = ["classic-music", "dance-music", "relax-music", "top-music" ]
+    
+    let colums = [
+        GridItem(.flexible())
+    ]
+    
+    var body: some View {
+        LazyVGrid(columns: colums, alignment: .leading) {
+            
+            ForEach(model, id: \.self) { model in
+                HStack(spacing: 20) {
+                    Image(model)
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 100, height: 100)
+                        .cornerRadius(5)
+                    VStack {
+                        Text(model)
+                            .font(.title3)
+                        Text(model)
+                            .font(.callout)
+                            .foregroundColor(Color.gray)
+                    }
+                }
+                Divider()
+            }
+            .padding(.horizontal, 20)
+            
+        }
+    }
+}
+
+
+
+struct StationsView_Previews: PreviewProvider {
+    static var previews: some View {
+        StationsView()
+    }
+}
