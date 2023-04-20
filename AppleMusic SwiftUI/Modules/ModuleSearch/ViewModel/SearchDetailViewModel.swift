79c19bb0b7a6555a23625818ba902e107e5c0ea3
Daniil Davidovich <dd.jopgm@gmail.com>
2023-04-20 05:36:18 +0300

    create SearchDetailAdditionalViewModel

--- AppleMusic SwiftUI/Modules/ModuleSearch/ViewModel/SearchDetailViewModel.swift
+++ AppleMusic SwiftUI/Modules/ModuleSearch/ViewModel/SearchDetailViewModel.swift
@@ -43,14 +43,73 @@ class SearchDetailViewModel: ObservableObject {
     }
     
     func getPlaylistAdditionalViewModel() {
-        
+        let newData = [
+            
+            SearchModel(title: "I Miss Russia Super Hits",
+                        description: "Apple Russia",
+                        image: "iMissRussia-additionalPlayslists"),
+            
+            SearchModel(title: "Soviet Movie Songs",
+                        description: "Apple Russia",
+                        image: "sovietMovieSongs-additionalPlayslists"),
+            
+            SearchModel(title: "Pop Wave",
+                        description: "Apple Russia",
+                        image: "popWave-additionalPlayslists"),
+            
+            SearchModel(title: "Pop Workout in Russia",
+                        description: "Apple Russia",
+                        image: "popWorkout-additionalPlayslists"),
+            
+            SearchModel(title: "Russian Love Songs",
+                        description: "Apple Russia",
+                        image: "russianLoveSongs-additionalPlayslists"),
+            
+        ]
+        playlistAdditionalViewModel.append(contentsOf: newData)
     }
     
     func getHitsAdditionalViewModel() {
-        
+        let newData = [
+            
+            SearchModel(title: "10's Pop In Russian",
+                        description: "Apple Music 2010s",
+                        image: "10's-additionalHits"),
+            
+            SearchModel(title: "00's Pop In Russian",
+                        description: "Apple Music 2000s",
+                        image: "00's-additionalHits"),
+            
+            SearchModel(title: "90's Pop In Russian",
+                        description: "Apple Music 90's",
+                        image: "90's-additionalHits"),
+            
+            SearchModel(title: "80's Pop In Russian",
+                        description: "Apple Music 80's",
+                        image: "80's-additionalHits"),
+            
+        ]
+        hitsAdditionalViewModel.append(contentsOf: newData)
     }
     
     func getAlbomsAdditionalViewModel() {
-        
+        let newData = [
+            SearchModel(title: "Millenium X",
+                        description: "Artik & Asti",
+                        image: "maleniumX-additionalAlboms"),
+            SearchModel(title: "Anima",
+                        description: "The Limba",
+                        image: "anima-additionalAlboms"),
+            SearchModel(title: "Sverhnova",
+                        description: "Suzanna",
+                        image: "sverhnova-additionalAlboms"),
+            SearchModel(title: "Vinyl #2",
+                        description: "Zivert",
+                        image: "vinyl#2-additionalAlboms"),
+            SearchModel(title: "Tribiut",
+                        description: "Varioust Artists",
+                        image: "tribiut-additionalAlboms"),
+        ]
+        albomsAdditionalViewModel.append(contentsOf: newData)
     }
 }
